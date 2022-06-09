ebba878e67b723665642fa4df7f210abd08d94c0
jukio330 <jukiya.shimakawa@nextmode.co.jp>
2022-06-09 14:19:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          cicd study aaa
+          cicd study hogehoge
         </p>
         <a
           className="App-link"
