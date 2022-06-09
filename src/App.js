ab8d3f33f43e20528d79b0fc47473abb795aabde
jukio330 <jukiya.shimakawa@nextmode.co.jp>
2022-06-09 12:02:27 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './nm_l_color.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          cicd study aaa
         </p>
         <a
           className="App-link"
